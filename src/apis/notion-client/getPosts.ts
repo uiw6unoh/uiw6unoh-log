9fd1ef42cf972928f2b8b3506b4aac9690ca6be4
Tora-data <twq41053@kw.ac.kr>
2025-03-12 17:29:32 +0900

    Update getPosts.ts

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -6,10 +6,12 @@ import getAllPageIds from "src/libs/utils/notion/getAllPageIds"
 import getPageProperties from "src/libs/utils/notion/getPageProperties"
 import { TPosts } from "src/types"
 
+// TODO: 원하는 slug들을 이 배열에 추가하여 제외
+const EXCLUDE_SLUGS = ["baekjoon-2743", "baekjoon-10798"]
+
 /**
  * @param {{ includePages: boolean }} - false: posts only / true: include pages
  */
-
 // TODO: react query를 사용해서 처음 불러온 뒤로는 해당데이터만 사용하도록 수정
 export const getPosts = async () => {
   let id = CONFIG.notionConfig.pageId as string
@@ -36,17 +38,16 @@ export const getPosts = async () => {
 
     const data = []
     for (let i = 0; i < pageIds.length; i++) {
-      const id = pageIds[i]
-      const properties =
-        (await getPageProperties(id, wholeBlocks, schema)) || null
-      if (!wholeBlocks[id]) continue
+      const pageId = pageIds[i]
+      const properties = (await getPageProperties(pageId, wholeBlocks, schema)) || null
+      if (!wholeBlocks[pageId]) continue
 
       // Add fullwidth, createdtime to properties
       properties.createdTime = new Date(
-        wholeBlocks[id].value?.created_time
+        wholeBlocks[pageId].value?.created_time
       ).toString()
       properties.fullWidth =
-        (wholeBlocks[id].value?.format as any)?.page_full_width ?? false
+        (wholeBlocks[pageId].value?.format as any)?.page_full_width ?? false
 
       data.push(properties)
     }
@@ -58,7 +59,11 @@ export const getPosts = async () => {
       return dateB - dateA
     })
 
-    const posts = data as TPosts
+    let posts = data as TPosts
+
+    // 1) 슬러그 제외 로직: 특정 슬러그들은 빌드 대상에서 제외
+    posts = posts.filter((post) => !EXCLUDE_SLUGS.includes(post.slug))
+
     return posts
   }
 }
